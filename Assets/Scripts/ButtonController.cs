ddcb4960edef231ab5f3e8604d1c6ab25e6f0761
WO-Nagano Tamawo <nagano.tamawo77@gmail.com>
2021-11-15 12:12:07 +0900

    リファクタリング

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -3,39 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ButtonController : MonoBehaviour
-{
-    public GameManager GameManager { get; set; }
-    public int Number { get; private set; }
+public class ButtonController : MonoBehaviour {
+    int number;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        this.GameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        this.GetComponent<Button>().onClick.AddListener(OnClick);
+    void Start() {
+        //this.GameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void OnClick()
-    {
-        if(this.GameManager.CheckNumber(Number)){
+    public void OnClick() {
+
+        Debug.Log(GameManager.CheckNumber(number));
+
+        if(GameManager.CheckNumber(number)){
             //正しい番号なら数字を進める
-            this.GameManager.ChangeNextValue();
+            GameManager.ChangeNextValue();
             Destroy(gameObject);
-        }
-        //もし正しい番号じゃなかったら、というのの書き方がわからない。
-        else{
+
+        } else {
             Debug.Log("damn it!");
         }
     }
+
     //ボタンにナンバーを設定
-    public void SetButtonInfo(int number)
-    {
-        this.Number = number;
+    public void SetButtonInfo(int allocatedNumber) {
+        number = allocatedNumber;
     }
 
 
